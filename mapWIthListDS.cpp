38b7f54aac96b2fd11315770824d761728928a90
PramithaMJ <lpramithamj@gmail.com>
2023-06-23 01:43:50 +0530

    Map with list

--- mapWIthListDS.cpp
+++ mapWIthListDS.cpp
@@ -6,5 +6,9 @@
 using namespace std;
 
 int main(){
+    map<string, list<string>> pokedex;
 
+    list<string> pikachuAttacks{"thunder shock", "tail whip","quick attact"};
+    list<string> charmanderAttacks{"flame throer", "scary face"};
+    list<string>chickeritauAttacks{"razor leaf", "poison powder"};
 }
