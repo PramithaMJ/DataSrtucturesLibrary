249e9499b2a529a093515c05c2d2d2112e90c63b
PramithaMJ <lpramithamj@gmail.com>
2023-06-23 01:25:42 +0530

    Map

--- mapDS.cpp
+++ mapDS.cpp
@@ -6,4 +6,10 @@ using namespace std;
 
 int main(){
     
+    map<string, string> myDictionary;
+    
+    myDictionary.insert(pair<string, string>("apple","Apple"));
+    myDictionary.insert(pair<string, string>("banana","kesel"));
+    myDictionary.insert(pair<string, string>("orange","thsbili, dodam"));
+
 }
